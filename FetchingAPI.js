9f8398d28827c7346729d5abbdc88f8da73598a5
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-12 20:23:51 +0200

    fetching API

--- FetchingAPI.js
+++ FetchingAPI.js
@@ -1,6 +1,16 @@
 async function logMovies() {
-  const response = await fetch("http://example.com/movies.json");
-  const movies = await response.json();
-  console.log(movies);
+  try {
+    const response = await fetch(
+      "https://github.com/gloriaumutoni?tab=repositories"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const movies = await response.json();
+    console.log(movies);
+  } catch (error) {
+    console.error("Failed to fetch movies:", error);
+  }
 }
+
 logMovies();
