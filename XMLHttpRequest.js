0055cba661361dc3937790872ed7b6ab77d395ef
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-13 19:50:31 +0200

    promises

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -1,34 +1,54 @@
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest(); //
-  request.addEventListener("readystatechange", () => {
-    //   console.log(request, request.readyStatechange);
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback("could not fetch data", undefined);
-    }
-  });
-
-  request.open("GET", resource);
-  request.send();
-};
-// promise example
-const getSomething = () => {
+const getTodos = (resource) => {
   return new Promise((resolve, reject) => {
-    // fetch getSomething
-    resolve("some data");
-    reject("some error");
+    const request = new XMLHttpRequest(); // making an object request
+    request.addEventListener("readystatechange", () => {
+      //   console.log(request, request.readyStatechange);
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+        // callback(undefined, data);
+      } else if (request.readyState === 4) {
+        reject("error getting resource");
+        // callback("could not fetch data", undefined);
+      }
+    });
+
+    request.open("GET", resource);
+    request.send();
   });
 };
-getSomething().then(
-  (data) => {
-    console.log(ReferenceError());
-  },
-  (err) => {
-    console.log("error");
-  }
-);
+getTodos("todos/luigi.json")
+  .then((data) => {
+    console.log("promise resolved", data);
+  })
+  .then((err) => {
+    console.log("promise rejected", err);
+  });
+// promise example
+// const getSomething = () => {
+//   return new Promise((resolve, reject) => {
+//     // fetch getSomething
+//     resolve("some data");
+//     reject("some error");
+//   });
+// };
+// getSomething().then(
+//   (data) => {
+//     console.log(ReferenceError());
+//   },
+//   (err) => {
+//     console.log("error");
+//   }
+// );
+
+// an alternative for then and catch
+// getSomething()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // getting data from json files
 // getTodos("todos/luigi.json", (err, data) => {
