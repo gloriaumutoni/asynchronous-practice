630b74eb8dd3b7b65a731f116a054f64774975fa
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-13 12:04:08 +0200

    checking for the status of the request

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -1,6 +1,10 @@
-const request = new XMLHttpRequest(); //making request object
+const request = new XMLHttpRequest(); //
 request.addEventListener("readystatechange", () => {
-  console.log(request, request.readyStatechange);
+  //   console.log(request, request.readyStatechange);
+  if (request.readyState === 4 && request.status === 200) {
+    console.log(request, request.responseText);
+  }
 });
+
 request.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
 request.send();
