f0fdb1617fd6ee905696d1ef0523b1410bb5421d
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-14 11:42:14 +0200

    practicing callbscs and XMLHttRequests

---
+++ practice.js
@@ -0,0 +1,10 @@
+let request = new XMLHttpRequest();
+request.addEventListener("readystatechange", () => {
+  if (request.readyState === 4 && request.status === 200) {
+    console.log(request.responseText);
+  } else if (request.readyState === 4) {
+    console.log("error occured");
+  }
+});
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+request.send();
