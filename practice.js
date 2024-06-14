d63d87f6beb79ad9b634e7247896a650e1f9bca8
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-14 12:11:20 +0200

    catching errors and data using callback

--- practice.js
+++ practice.js
@@ -1,10 +1,22 @@
-let request = new XMLHttpRequest();
-request.addEventListener("readystatechange", () => {
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText);
-  } else if (request.readyState === 4) {
-    console.log("error occured");
+const doSomethng = (callback) => {
+  let request = new XMLHttpRequest();
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+      //   console.log(request.responseText);
+    } else if (request.readyState === 4) {
+      callback("erro fired", undefined);
+      //   console.log("error occured");
+    }
+  });
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.send();
+};
+doSomethng((err, data) => {
+  console.log("callback fired");
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
   }
 });
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-request.send();
