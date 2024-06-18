93725d98fa3ab334389cfb7c77a526801de9b494
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-18 14:36:47 +0200

    aync & await

--- aync&await.js
+++ aync&await.js
@@ -1,5 +1,18 @@
 // aync&await
 
+const getTodos = async (responses) => {
+  responses = await fetch("todos/luigi.json");
+  const data = await responses.json();
+  return data;
+};
+getTodos()
+  .then((data) => {
+    console.log("resolved", data);
+  })
+  .catch((err) => {
+    console.log("rejected", err);
+  });
+
 // fetch("todos/luigi.json")
 //   .then((response) => {
 //     console.log(response);
