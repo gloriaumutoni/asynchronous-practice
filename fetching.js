7c36987452e9d6de72d2c412ffb2d0c530c3e191
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-18 11:55:58 +0200

    fetching API

---
+++ fetching.js
@@ -0,0 +1,11 @@
+fetch("todos/luigi.json")
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
