5c4e6443ca73253e81937560e8fb304b4a111736
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-19 20:36:56 +0200

    time limit

--- timeLimit.js
+++ timeLimit.js
@@ -1,7 +1,7 @@
 // 1. Write a JavaScript function that takes a callback and invokes it after a delay of 2 second.
 function delay(ms) {
   setTimeout(() => {
-    console.log("delay in milliseconds");
+    console.log("delay milliseconds");
   }, ms);
 }
 delay(2000);
