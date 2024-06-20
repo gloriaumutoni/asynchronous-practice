ebbd90564cd80224814fe9b24a3c1de34fef67a9
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-20 12:03:40 +0200

    adding callbacks to the then on the promise

--- promises/promise.js
+++ promises/promise.js
@@ -1,9 +1,17 @@
-let cart = ["shoes", "cargo", "kart"];
+// let cart = ["shoes", "cargo", "kart"];
 
-createorder(cart, (orderId) => {
-  proceedToPayment(orderId);
-});
-let promise = createorder(cart);
-promise.then((orderId) => {
-  proceedToPayment(orderId);
+// createorder(cart, (orderId) => {
+//   proceedToPayment(orderId);
+// });
+
+// let promise = createorder(cart);
+// promise.then((orderId) => {
+//   proceedToPayment(orderId);
+// });
+
+let GITHUB_USER = "https://github.com/gloriaumutoni";
+let user = fetch("https://github.com/gloriaumutoni");
+console.log(user);
+user.then((data) => {
+  console.log(data);
 });
