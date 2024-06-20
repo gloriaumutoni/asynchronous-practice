28c2e2ba7b1c949fbcec77f970ec507709e1baa9
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-20 13:43:02 +0200

    js promises

--- promises/promise.js
+++ promises/promise.js
@@ -1,17 +1,17 @@
-// let cart = ["shoes", "cargo", "kart"];
+let cart = ["shoes", "cargo", "kart"];
 
-// createorder(cart, (orderId) => {
-//   proceedToPayment(orderId);
-// });
-
-// let promise = createorder(cart);
-// promise.then((orderId) => {
-//   proceedToPayment(orderId);
-// });
-
-let GITHUB_USER = "https://github.com/gloriaumutoni";
-let user = fetch(GITHUB_USER);
-console.log(user);
-user.then(function (data) {
-  console.log(data);
+createorder(cart, (orderId) => {
+  proceedToPayment(orderId);
 });
+
+let promise = createorder(cart);
+promise.then((orderId) => {
+  proceedToPayment(orderId);
+});
+
+// let GITHUB_USER = "https://github.com/gloriaumutoni";
+// let user = fetch(GITHUB_USER);
+// console.log(user);
+// user.then(function (data) {
+//   console.log(data);
+// });
