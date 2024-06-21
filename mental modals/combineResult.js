8eed533ae3d1ce5a5b67e66bd0410a54bf49a9c2
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-21 12:37:44 +0200

    fetches data from multiple APIs concurrently

--- mental modals/combineResult.js
+++ mental modals/combineResult.js
@@ -1,14 +1,22 @@
 function fetchMultipleAPIs(apiUrls) {
-  //   let promise1 = fetch(apiUrls[0]);
-  //   let promise2 = fetch(apiUrls[1]);
-  //   let promise3 = fetch(apiUrls[2]);
-  let promise = [];
-  apiUrls.map((url) => {
-    promise.push(url);
-  });
-  return Promise.all(promise);
-  //   return Promise.all([promise1, promise2, promise3]);
+  // let fetch= async(url)=>{
+  let fetch = async (url) => {
+    let fetching = await fetch(url);
+    let response = await fetching.json();
+    return response;
+  };
+  return Promise.all(apiUrls.map((url) => fetch(url)));
 }
+//   let promise1 = fetch(apiUrls[0]);
+//   let promise2 = fetch(apiUrls[1]);
+//   let promise3 = fetch(apiUrls[2]);
+//   //   let promise = [];
+//   //   apiUrls.map((url) => {
+//   //     promise.push(url);
+//   //   });
+//   //   return Promise.all(promise);
+//   return Promise.all([promise1, promise2, promise3]);
+// }
 
 const apiUrls = [
   "https://jsonplaceholder.typicode.com/posts/4",
