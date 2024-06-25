bc4fed8a3532c0e374b6a7533bdfa739bed89bea
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-25 20:09:46 +0200

    fetching API from country code

--- mental modals/sixth.js
+++ mental modals/sixth.js
@@ -1,2 +1,28 @@
-function fetch(){}
-fetch()
+async function myfetch(countryUrl,weatherUrl){
+    try{
+        // fetch the url 
+        let countryResponse=await fetch(countryUrl)
+        let weatherResponse=await fetch(weatherUrl)
+
+        // convert url into jsonValues
+        let countryJson
+        let weatherJson
+        if(countryResponse.ok){
+           countryJson=await countryResponse.json()
+        }
+        else if(weatherResponse.ok){
+            weatherJson=await weatherResponse.json()
+        }
+        else{
+            throw new Error('not valid!!!')
+        }
+            console.log(countryJson,weatherJson)
+        //get country,latitude and longitude 
+
+    }catch(err){
+        console.log('Error:',err);
+    }
+    
+
+}
+myfetch('https://restcountries.com/v3.1/name/Rwanda',' https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true')
