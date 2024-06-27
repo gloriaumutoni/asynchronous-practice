3c82695c22ffff7a2877b121d61d6aab45466bce
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-27 14:17:59 +0200

    fetching data from multiple APIs to display information about different countries.

--- mental modals/sixth.js
+++ mental modals/sixth.js
@@ -1,28 +1,34 @@
-async function myfetch(countryUrl,weatherUrl){
+async function myfetch(country){
     try{
         // fetch the url 
+        let countryUrl=`https://restcountries.com/v3.1/name/${country}`
         let countryResponse=await fetch(countryUrl)
-        let weatherResponse=await fetch(weatherUrl)
+        
 
         // convert url into jsonValues
-        let countryJson
-        let weatherJson
+        let countryJson,weatherJson,lat,lon
+        // let weatherJson
         if(countryResponse.ok){
            countryJson= await countryResponse.json()
+           lat=countryJson[0].latlng[0]
+           lon=countryJson[0].latlng[1]
         }
-        else if(weatherResponse.ok){
+        let weatherResponse=await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`)
+         if(weatherResponse.ok){
             weatherJson=await weatherResponse.json()
         }
         else{
             throw new Error('not valid!!!')
-        }
-            console.log(countryJson)
-        //get country,latitude and longitude 
-
+        } 
+        let Capital=countryJson[0].capital[0]
+        let Current_Temperature=weatherJson.current_weather.temperature
+        console.log(`Country: ${country}
+Capital: ${Capital}
+Current Temperature: ${Current_Temperature}°C`)
     }catch(err){
         console.log('Error:',err);
     }
     
 
 }
-myfetch('https://restcountries.com/v3.1/name/Rwanda',' https://api.open-meteo.com/v1/forecast?latitude=-2&longitude=30&current_weather=true')
+myfetch('France')
