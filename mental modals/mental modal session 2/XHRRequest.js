4ca3af1ab4d5e26a0f498ebcb20a337943dd18e9
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-30 22:43:08 +0200

    updated README.md

--- mental modals/mental modal session 2/XHRRequest.js
+++ mental modals/mental modal session 2/XHRRequest.js
@@ -2,27 +2,32 @@
 let btn=document.querySelector('button')
 
 function fetchToDo(){
-    let xhr=new XMLHttpRequest()
-    xhr.open("POST",'https://jsonplaceholder.typicode.com/todos/1')
-    xhr.setRequestHeader('Content-Type','application/json')
-    xhr.onload=function(){
-        if(xhr.readyState===4 && xhr.status===200){
-           let responseJson=JSON.parse(xhr.responseText) 
-           console.log(responseJson)
+    try{
+        let xhr=new XMLHttpRequest()
+        xhr.open("POST",'https://jsonplaceholder.typicode.com/todos/1')
+        xhr.setRequestHeader('Content-Type','application/json')
+        xhr.onload=function(){
+            if(xhr.readyState===4 && xhr.status===200){
+               let responseJson=JSON.parse(xhr.responseText) 
+               console.log(responseJson)
+            }
+            else{
+               console.log('network failed') 
+            }
         }
-        else{
-           console.log('network failed') 
+        xhr.onerror=function(){
+            console.log('network failed')
         }
+        let data=JSON.stringify({
+            title: "foo",
+            body: "bar",
+            userId: 1
+        })
+        xhr.send(data)
+        console.log(data)
+    }catch(err){
+        console.log(err)
     }
-    xhr.onerror=function(){
-        console.log('network failed')
-    }
-    let data=JSON.stringify({
-        title: "foo",
-        body: "bar",
-        userId: 1
-    })
-    xhr.send(data)
 }
 
 btn.addEventListener('click',fetchToDo())
